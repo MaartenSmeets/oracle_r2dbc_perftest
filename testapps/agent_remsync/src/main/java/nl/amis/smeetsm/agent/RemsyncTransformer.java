2522f271037295a093d5e3b8a8efb1fd30648102
Maarten Smeets <wisp1980@gmail.com>
2021-02-12 16:37:59 +0100

    Work in progress creating instrumentation

--- testapps/agent_remsync/src/main/java/nl/amis/smeetsm/agent/RemsyncTransformer.java
+++ testapps/agent_remsync/src/main/java/nl/amis/smeetsm/agent/RemsyncTransformer.java
@@ -1,60 +1,46 @@
 package nl.amis.smeetsm.agent;
 
 import javassist.*;
+import javassist.scopedpool.ScopedClassPoolFactoryImpl;
+import javassist.scopedpool.ScopedClassPoolRepositoryImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
-import java.util.Arrays;
-import java.util.List;
 
 public class RemsyncTransformer implements ClassFileTransformer {
 
-    private static Logger LOGGER = LoggerFactory.getLogger(RemsyncTransformer.class);
-
-    /** The internal form class name of the class to transform */
-    private String targetClassName;
-    /** The class loader of the class we want to transform */
-    private ClassLoader targetClassLoader;
-
-    public RemsyncTransformer(String targetClassName, ClassLoader targetClassLoader) {
-        this.targetClassName = targetClassName;
-        this.targetClassLoader = targetClassLoader;
-    }
+    private static final Logger LOGGER = LoggerFactory.getLogger(RemsyncTransformer.class);
+    private ScopedClassPoolFactoryImpl scopedClassPoolFactory = new ScopedClassPoolFactoryImpl();
 
     @Override
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
                             ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
         byte[] byteCode = classfileBuffer;
-
-        String finalTargetClassName = this.targetClassName.replaceAll("\\.", "/"); //replace . with /
-        if (!className.equals(finalTargetClassName)) {
-            return byteCode;
-        }
-
-        if (className.equals(finalTargetClassName) && loader.equals(targetClassLoader)) {
-            LOGGER.info("[Agent] Transforming class");
-            int modifier = 0;
-            try {
-                ClassPool cp = ClassPool.getDefault();
-                CtClass cc = cp.get(targetClassName);
-                List<CtMethod> methods = Arrays.asList(cc.getMethods());
-                for (CtMethod method : methods) {
-                    modifier = method.getModifiers();
-                    if (Modifier.isSynchronized(modifier)) {
-                        LOGGER.info("Method is synchronized: " + method.getName());
-                        modifier = Modifier.clear(modifier,Modifier.SYNCHRONIZED);
-                        method.setModifiers(modifier);
-                    }
+        LOGGER.info("[Agent] Transforming class: " + className);
+        int modifier = 0;
+        try {
+            ClassPool classPool = scopedClassPoolFactory.create(loader, ClassPool.getDefault(),
+                    ScopedClassPoolRepositoryImpl.getInstance());
+            //ClassPool classPool = ClassPool.getDefault();
+            CtClass ctClass = classPool.makeClass(new ByteArrayInputStream(classfileBuffer));
+            CtMethod[] methods = ctClass.getMethods();
+            for (CtMethod method : methods) {
+                modifier = method.getModifiers();
+                if (Modifier.isSynchronized(modifier)) {
+                    LOGGER.info("Method is synchronized: " + method.getName());
+                    modifier = Modifier.clear(modifier, Modifier.SYNCHRONIZED);
+                    method.setModifiers(modifier);
                 }
-                byteCode = cc.toBytecode();
-                cc.detach();
-            } catch (NotFoundException | CannotCompileException | IOException e) {
-                LOGGER.error("Exception", e);
             }
+            byteCode = ctClass.toBytecode();
+            ctClass.detach();
+        } catch (CannotCompileException | IOException e) {
+            LOGGER.error("Exception", e);
         }
         return byteCode;
     }
