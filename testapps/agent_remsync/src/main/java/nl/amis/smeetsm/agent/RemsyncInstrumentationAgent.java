3550daf5733d86cdb923a50c5aa34dbc1a21c788
Maarten Smeets <wisp1980@gmail.com>
2021-02-13 11:02:39 +0100

    Removed most logging. Excluded java.* packages

--- testapps/agent_remsync/src/main/java/nl/amis/smeetsm/agent/RemsyncInstrumentationAgent.java
+++ testapps/agent_remsync/src/main/java/nl/amis/smeetsm/agent/RemsyncInstrumentationAgent.java
@@ -1,20 +1,20 @@
 package nl.amis.smeetsm.agent;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.lang.instrument.Instrumentation;
 
 public class RemsyncInstrumentationAgent {
-    private static final Logger LOGGER = LoggerFactory.getLogger(RemsyncInstrumentationAgent.class);
+
+    private static void log(String line) {
+        System.out.println(RemsyncInstrumentationAgent.class.getName().toString()+" : "+line);
+    }
 
     public static void premain(String agentArgs, Instrumentation inst) {
-        LOGGER.info("[Agent] In premain method");
+        log("[Agent] In premain method");
         inst.addTransformer(new RemsyncTransformer());
     }
 
     public static void agentmain(String agentArgs, Instrumentation inst) {
-        LOGGER.info("[Agent] In agentmain method");
+        log("[Agent] In agentmain method");
         inst.addTransformer(new RemsyncTransformer());
     }
 
--- testapps/agent_remsync/src/main/java/nl/amis/smeetsm/agent/RemsyncTransformer.java
+++ testapps/agent_remsync/src/main/java/nl/amis/smeetsm/agent/RemsyncTransformer.java
@@ -5,9 +5,6 @@ import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
 import javassist.scopedpool.ScopedClassPoolFactoryImpl;
 import javassist.scopedpool.ScopedClassPoolRepositoryImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
@@ -15,63 +12,68 @@ import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 
 public class RemsyncTransformer implements ClassFileTransformer {
+    private static void log(String line) {
+        System.out.println(RemsyncTransformer.class.getName().toString()+" : "+line);
+    }
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(RemsyncTransformer.class);
     private final ScopedClassPoolFactoryImpl scopedClassPoolFactory = new ScopedClassPoolFactoryImpl();
 
     @Override
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
                             ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
         byte[] byteCode = classfileBuffer;
-        LOGGER.info("[Agent] Transforming class: " + className);
-        int modifier = 0;
-        try {
-            ClassPool classPool = scopedClassPoolFactory.create(loader, ClassPool.getDefault(),
-                    ScopedClassPoolRepositoryImpl.getInstance());
-            //ClassPool classPool = ClassPool.getDefault();
-            CtClass ctClass = classPool.makeClass(new ByteArrayInputStream(classfileBuffer));
-            CtMethod[] methods = ctClass.getMethods();
-            CtField ctField = null;
-            for (CtMethod method : methods) {
-                modifier = method.getModifiers();
-                if (Modifier.isSynchronized(modifier)) {
-                    LOGGER.info("[Agent] Method is synchronized: " + method.getName());
-                    if (Modifier.isStatic(modifier)) {
-                        //https://paluch.biz/blog/183-carrier-kernel-thread-pinning-of-virtual-threads-project-loom.html
-                        try {
-                            ctField = ctClass.getDeclaredField("lockCustomAgentStatic");
-                        } catch (NotFoundException e) {
-                            ctClass.addField(CtField.make("final static java.util.concurrent.locks.Lock lockCustomAgentStatic = new java.util.concurrent.locks.ReentrantLock();", ctClass));
-                        }
-
-                        //https://stackoverflow.com/questions/41156596/using-javassist-to-insert-try-finally-logic-that-wraps-the-original-method-logic
-                        method.instrument(new ExprEditor() {
-                            public void edit(MethodCall m) throws CannotCompileException {
-                                m.replace("{ lockCustomAgentStatic.lock(); try { $_ = $proceed($$); } finally { lockCustomAgentStatic.unlock(); } }");
+        if (!className.startsWith("java/")) {
+            //log("[Agent] Transforming class: " + className);
+            int modifier = 0;
+            try {
+                ClassPool classPool = scopedClassPoolFactory.create(loader, ClassPool.getDefault(),
+                        ScopedClassPoolRepositoryImpl.getInstance());
+                //ClassPool classPool = ClassPool.getDefault();
+                CtClass ctClass = classPool.makeClass(new ByteArrayInputStream(classfileBuffer));
+                CtMethod[] methods = ctClass.getMethods();
+                CtField ctField = null;
+                for (CtMethod method : methods) {
+                    modifier = method.getModifiers();
+                    if (Modifier.isSynchronized(modifier)) {
+                        //log("[Agent] Method is synchronized: " + method.getName());
+                        if (Modifier.isStatic(modifier)) {
+                            //https://paluch.biz/blog/183-carrier-kernel-thread-pinning-of-virtual-threads-project-loom.html
+                            try {
+                                ctField = ctClass.getDeclaredField("lockCustomAgentStatic");
+                            } catch (NotFoundException e) {
+                                ctClass.addField(CtField.make("final static java.util.concurrent.locks.Lock lockCustomAgentStatic = new java.util.concurrent.locks.ReentrantLock();", ctClass));
                             }
-                        });
-                    } else {
-                        try {
-                            ctField = ctClass.getDeclaredField("lockCustomAgent");
-                        } catch (NotFoundException e) {
-                            ctClass.addField(CtField.make("final java.util.concurrent.locks.Lock lockCustomAgent = new java.util.concurrent.locks.ReentrantLock();", ctClass));
-                        }
 
-                        method.instrument(new ExprEditor() {
-                            public void edit(MethodCall m) throws CannotCompileException {
-                                m.replace("{ lockCustomAgent.lock(); try { $_ = $proceed($$); } finally { lockCustomAgent.unlock(); } }");
+                            //https://stackoverflow.com/questions/41156596/using-javassist-to-insert-try-finally-logic-that-wraps-the-original-method-logic
+                            method.instrument(new ExprEditor() {
+                                public void edit(MethodCall m) throws CannotCompileException {
+                                    m.replace("{ lockCustomAgentStatic.lock(); try { $_ = $proceed($$); } finally { lockCustomAgentStatic.unlock(); } }");
+                                }
+                            });
+                        } else {
+                            try {
+                                ctField = ctClass.getDeclaredField("lockCustomAgent");
+                            } catch (NotFoundException e) {
+                                ctClass.addField(CtField.make("final java.util.concurrent.locks.Lock lockCustomAgent = new java.util.concurrent.locks.ReentrantLock();", ctClass));
                             }
-                        });
+
+                            method.instrument(new ExprEditor() {
+                                public void edit(MethodCall m) throws CannotCompileException {
+                                    m.replace("{ lockCustomAgent.lock(); try { $_ = $proceed($$); } finally { lockCustomAgent.unlock(); } }");
+                                }
+                            });
+                        }
+                        modifier = Modifier.clear(modifier, Modifier.SYNCHRONIZED);
+                        method.setModifiers(modifier);
                     }
-                    modifier = Modifier.clear(modifier, Modifier.SYNCHRONIZED);
-                    method.setModifiers(modifier);
                 }
-            }
 
-            byteCode = ctClass.toBytecode();
-            ctClass.detach();
-        } catch (CannotCompileException | IOException e) {
-            LOGGER.info("[Agent] Failed to transform: "+className);
+                byteCode = ctClass.toBytecode();
+                ctClass.detach();
+                //log("[Agent] Class transformed: "+className);
+            } catch (Exception e) {
+                //log("[Agent] Failed to transform: "+className+ " Cause: "+e.getMessage());
+            }
         }
         return byteCode;
     }
